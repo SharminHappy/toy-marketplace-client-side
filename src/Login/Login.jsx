24dc4217d7e8351d0fadc9de9e42bd5e10e1c0ec
sharmin akter <sharmin.happy.25@gmail.com>
2023-05-18 07:26:18 +0600

    login page created

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
@@ -6,8 +7,8 @@ const Login = () => {
     }
 
     return (
-        <div className="hero min-h-screen ">
-            <div className="card  w-full max-w-sm shadow-2xl ">
+        <div className="hero min-h-screen">
+            <div className="card  w-full max-w-sm shadow-2xl">
                 <h1 className="text-3xl text-center font-bold text-pink-300">Please Login !</h1>
                 <form onSubmit={handleLogin} className="card-body">
                     <div className="form-control">
@@ -26,6 +27,7 @@ const Login = () => {
                         <input className="btn btn-outline hover:border-info hover:bg-info bg-blue-200" type="submit" value='Login'/>
                     </div>
                 </form>
+                <p className="text-center my-4">Doesn't have an account yet?<Link to='/signup' className="text-pink-300 font-bold">Sign Up</Link></p>
             </div>
         </div>
 
